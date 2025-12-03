fd07a31de3921d699e4da525890d24a3061693f0
viniciusrbr <viniciusgoes159@gmail.com>
2025-12-03 16:23:32 -0300

    fix: css e porta de entrada

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,3 +1,7 @@
+import tailwindcss from '@tailwindcss/postcss'
+import { readFile, writeFile } from 'fs/promises'
+import { resolve } from 'path'
+import postcss from 'postcss'
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
@@ -9,4 +13,22 @@ export default defineConfig({
   sourcemap: false,
   clean: true,
   tsconfig: 'tsconfig.build.json',
+  esbuildOptions(options) {
+    options.banner = {
+      js: '"use client"',
+    }
+  },
+  async onSuccess() {
+    const cssPath = resolve('src/index.css')
+    const outPath = resolve('dist/index.css')
+
+    const css = await readFile(cssPath, 'utf8')
+    const result = await postcss([tailwindcss()]).process(css, {
+      from: cssPath,
+      to: outPath,
+    })
+
+    await writeFile(outPath, result.css)
+    console.log('CSS processed with Tailwind CSS')
+  },
 })
